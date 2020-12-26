ceff0744cb9ca97c750ca4ecc8785bf9e01b9e02
semih-git <cemcan34355@gmail.com>
2020-12-26 23:36:36 +0100

    I edited Driver class.

--- src/main/java/org/cb/ta/Driver.java
+++ src/main/java/org/cb/ta/Driver.java
@@ -2,14 +2,29 @@ package org.cb.ta;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
 import java.util.concurrent.TimeUnit;
 
 public class Driver {
+
     public static WebDriver getdriver(){
-        System.setProperty("webdriver.chrome.driver","webdriver\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        return driver;
+        String browserType = "chrome";
+        switch (browserType){
+            case "chrome":
+                System.setProperty("webdriver.chrome.driver","webdriver\\chromedriver.exe");
+                return new ChromeDriver();
+            case "firefox":
+                System.setProperty("webdriver.gecko.driver","webdriver\\geckodriver.exe");
+                return new FirefoxDriver();
+            case "safari":
+                System.setProperty(" "," ");
+                return new SafariDriver();
+            default:
+                //if browser type is wrong, throw and exception showing us browser will not be openned.
+                throw new RuntimeException("Wrong browser type!");
+        }
     }
 }
